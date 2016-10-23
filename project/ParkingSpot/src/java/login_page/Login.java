50effc514f316cd8b7e12833b598a7674e41be97
root <karakoul@uth.gr>
2016-10-24 02:42:08 +0300

    test login page added

---
+++ project/ParkingSpot/src/java/login_page/Login.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_page;
+
+/**
+ *
+ * @author katerina
+ */
+import com.uthldap.Uthldap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Login extends HttpServlet
+{
+
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
+    {
+        String uid = req.getParameter( "uname" );
+        String passwd = req.getParameter( "psw" );
+
+        Uthldap uthldap = new Uthldap( uid, passwd );
+
+        if ( uthldap.auth() )
+        {
+            RequestDispatcher view = req.getRequestDispatcher( "index.jsp" );
+
+            view.forward( req, resp );
+        }
+        else
+        {
+            RequestDispatcher view;
+            view = req.getRequestDispatcher( "login.html" );
+
+            view.forward( req, resp );
+        }
+    }
+}
+
